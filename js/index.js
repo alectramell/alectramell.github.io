1cc857669c91029785479c7fad7dd403646f2d73
alectramell <alectramell@gmail.com>
2020-12-21 11:25:59 -0800

    alectramell

--- js/index.js
+++ js/index.js
@@ -16,6 +16,6 @@ function gotoFacebook() {
 
 function winStoreOpen() {
 
-	var win = window.open('https://alectramell.github.io/winstorw.hta');
+	var win = window.open('https://alectramell.github.io/winstore.hta');
 	win.focus();
 }
