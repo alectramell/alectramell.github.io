c05675f1fd0507c2eb4b0d4daecb8dd8849f22ed
alectramell <alectramell@gmail.com>
2020-12-21 11:03:38 -0800

    alectramell

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@ function reLoad(xvar) {
 
 	setTimeout(function(){location.reload();}, xvar);
 }
+
+function goTab(url) {
+
+	var win = window.open(url, '_blank');
+	win.focus();
+}
+
+function gotoFacebook() {
+
+	goTab('https://www.facebook.com/tramelldev');
+}
