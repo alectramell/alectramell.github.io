ad0dcb25d83c9c6c7a9170f6b48836c7d6dbc55d
Alec Tramell, DMA <alectramell@gmail.com>
2021-12-28 14:14:24 -0700

    da plugin

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ function goFullScreen() {
 	document.documentElement.requestFullscreen()
 }
 
+function sendDArequest() {
+
+    window.open('mailto:alectramell@gmail.com?subject=(TSD) Data Analyst Request&body=This is a potential Data Analyst request.%0d%0a%0d%0aTramell Software Development will reply to the email sending this request within a 12 hour business timeframe. If you would like to add any other information to the request (contact info, confirmation requirements, agent preferences, etc.) please add the information below before sending the request..%0d%0a');
+}
+
 function reLoad(xvar) {
 
 	setTimeout(function(){location.reload();}, xvar);
