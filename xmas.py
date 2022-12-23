cc9627de157b9161e1b1dc7889aed23c0c126a09
Alec Tramell, DMA <alectramell@gmail.com>
2022-12-23 15:32:25 -0700

    Create xmas.py

---
+++ xmas.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import shutil
+import cursor
+
+cursor.hide()
+
+class bcolors:
+    OKPURPLE = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    OKXMAS = '\033[37m\033[40m'
+    OKGOLD = '\033[93m'
+    OKBROWN = '\033[33m'
+    OKRED = '\033[91m'
+    OKWHITE = '\033[37m'
+    BKWHITE = '\033[47m'
+    OKSNOW = '\033[37m\033[47m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+cols = shutil.get_terminal_size().columns
+
+def start():
+
+	xtree = f'''
+
+                    {bcolors.OKGOLD}@{bcolors.ENDC}
+                    {bcolors.OKGREEN}*{bcolors.ENDC}
+                   {bcolors.OKGREEN}***{bcolors.ENDC}
+                  {bcolors.OKGREEN}**{bcolors.OKRED}o{bcolors.OKGREEN}**{bcolors.ENDC}
+                 {bcolors.OKGREEN}*****{bcolors.OKBLUE}o{bcolors.OKGREEN}*{bcolors.ENDC}
+                {bcolors.OKGREEN}*********{bcolors.ENDC}              {bcolors.OKXMAS}HAPPY HOLIDAYS!!!{bcolors.ENDC}
+               {bcolors.OKGREEN}**{bcolors.OKGOLD}o{bcolors.OKGREEN}***{bcolors.OKRED}o{bcolors.OKGREEN}****{bcolors.ENDC}                   {bcolors.OKXMAS}from{bcolors.ENDC}
+              {bcolors.OKGREEN}*************{bcolors.ENDC}       {bcolors.OKXMAS}Tramell Software Development{bcolors.ENDC}      
+             {bcolors.OKGREEN}**{bcolors.OKPURPLE}o{bcolors.OKGREEN}******{bcolors.OKRED}o{bcolors.OKGREEN}*****{bcolors.ENDC}
+            {bcolors.OKGREEN}******{bcolors.OKGOLD}o{bcolors.OKGREEN}*******{bcolors.OKPURPLE}o{bcolors.OKGREEN}**{bcolors.ENDC}
+           {bcolors.OKGREEN}************{bcolors.OKBLUE}o{bcolors.OKGREEN}******{bcolors.ENDC}
+          {bcolors.OKGREEN}***{bcolors.OKRED}o{bcolors.OKGREEN}*************{bcolors.OKGOLD}o{bcolors.OKGREEN}***{bcolors.ENDC}
+                   {bcolors.OKBROWN}###{bcolors.ENDC}
+                   {bcolors.OKBROWN}###{bcolors.ENDC}
+                   {bcolors.OKBROWN}###{bcolors.ENDC}
+{bcolors.OKSNOW}========================================================================{bcolors.ENDC}'''
+
+	os.system('clear')
+	print(f'{xtree}'.center(cols))
+	input();os.system('clear')
+
+start()
